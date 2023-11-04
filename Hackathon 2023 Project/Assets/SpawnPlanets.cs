9173efa658470b9ade3c7eac4b29407f7a47ada0
CoderXam <maxdaniellevine@gmail.com>
2023-11-04 16:56:22 -0400

    planet stuff

--- Hackathon 2023 Project/Assets/SpawnPlanets.cs
+++ Hackathon 2023 Project/Assets/SpawnPlanets.cs
@@ -6,6 +6,7 @@ public class SpawnPlanets : MonoBehaviour
 {
     int timeInterval = 3;
     public GameObject planet;
+    int previousTime;
 
     void Start()
     {
@@ -14,10 +15,11 @@ public class SpawnPlanets : MonoBehaviour
 
     void FixedUpdate()
     {
-        if ((int)Time.time % timeInterval == 0 && (int)Time.time != 0)
+        if ((int)Time.time % timeInterval == 0 && (int)Time.time != 0 && (int)Time.time != previousTime)
         { 
             Debug.Log("spawn planet");
             Instantiate(planet);
         }
+        previousTime = (int)Time.time;
     }
 }
