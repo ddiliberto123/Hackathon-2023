35aed82871520b36a7228397c89a3cf34c29a4da
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2023-11-04 18:01:11 -0400

    fixed player movement !
    fixed player movement

--- Hackathon 2023 Project/Assets/playerMovement.cs
+++ Hackathon 2023 Project/Assets/playerMovement.cs
@@ -7,45 +7,40 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public GameObject player;
-    public Sprite earth;
-    public int velocy, spacing;
-    float theta = Mathf.PI / 4; 
-    public float thetaChange;
-    double x, y;
-    private float expandedRadius;
+    public GameObject earth;
+    public float radius = 2.5f;
+    public float moveSpeed = 5f;
+    //make sure z is 0
+    private Vector3 center;
+    private float angle = 20.41114f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        //Gets the arc that the player will be moving along, using the the earth + spacing
-        //Spacing is / 100, to have more precision
-        expandedRadius = (float) (.5 + spacing/100);
-        Debug.Log(0);
+        center = new Vector3(0f, -5f, 0f); // Set the desired initial position
+        transform.position = center;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Handle keyboard input for left and right movement.
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        switch (Input.GetAxis("Horizontal"))
-        {
-            //Right
-            case 1:
-                if (theta == 0)
-                    break;
-                theta -= (thetaChange/360) * 2*Mathf.PI;
-                break;
-            //Left
-            case -1:
-                if (theta == Mathf.PI)
-                    break;
-                theta += (thetaChange/360) * 2*Mathf.PI;
-                break;
-            default:
-                break;
-        }
-        player.transform.position = new Vector2(expandedRadius * Mathf.Cos(theta), expandedRadius * Mathf.Sin(theta));
-        Debug.Log(Input.GetAxis("Horizontal") + ":" + theta + ":");
+        angle -= horizontalInput * moveSpeed * Time.deltaTime;
 
+        // Constrain the angle within the specified range.
+        angle = Mathf.Clamp(angle, 19.3f, 21.5f);
+
+        // Calculate the new position
+        float newX = center.x + Mathf.Cos(angle) * radius;
+        float newY = center.y + Mathf.Sin(angle) * radius;
+        Vector3 newPosition = new Vector3(newX, newY, transform.position.z);
+
+        // Update the position.
+        transform.position = newPosition;
+
+        // Calculate the z-rotation based on the movement angle.
+        float rotationAngle = Mathf.Rad2Deg * angle;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationAngle);
     }
 }
